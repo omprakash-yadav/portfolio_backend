e70450e0552ea98cafb2b0ad128f60c4208852e8
omprakash yadav <omprakash9597@gmail.com>
2022-07-30 12:19:39 +0530

    backend

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.download("./OP RESUME_22.pdf");
+ // res.download("./OP RESUME_22.pdf");
+ res.send("helo wld")
 });
 
 app.listen(4000);
